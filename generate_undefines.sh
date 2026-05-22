331e09ed5d7415702c862a0edd911a912589879e
Seng <16745114+Seng3694@users.noreply.github.com>
2026-05-22 15:08:12 +0200

    update script to catch 'defined(MACRO)'

--- generate_undefines.sh
+++ generate_undefines.sh
@@ -2,5 +2,8 @@
 
 # script used to retrieve all the macros used in a template header file and printing corresponding undefines
 # usage example: ./generate_undefines.sh src/hashmap.h
-grep -Po '(?<=^#(?:ifn?def|define) )([A-Z_][A-Z0-9_]*)' "$1" | sort -u | sed -r 's/^/#undef /'
+
+# grep -P '(?<=^#(?:ifn?def|define|if))' "$1" | grep -Po '[A-Z0-9_]{4,}' | sort -u | sed -r 's/^/#undef /' 
+
+grep -Po '(?<=(?:#ifn?def |#define |defined\())[A-Z][A-Z0-9_]*'  "$1" | sort -u | sed -r 's/^/#undef /' 
 
